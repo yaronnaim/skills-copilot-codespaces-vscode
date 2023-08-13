1fe05fb7eab81fef268ebbe3812d9dbdf022c148
Yaron Naim <41828060+yaronnaim@users.noreply.github.com>
2023-08-13 17:20:50 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,11 +1,20 @@
-function skillsMemeber() {
-    var skills = document.getElementById("skills").value;
-    var skillsArray = skills.split(",");
-    var skillsList = document.getElementById("skillsList");
-    skillsList.innerHTML = "";
-    for (var i = 0; i < skillsArray.length; i++) {
-        var li = document.createElement("li");
-        li.innerHTML = skillsArray[i];
-        skillsList.appendChild(li);
+function skillsMember() {
+  var member = document.getElementById("member");
+  var memberSkills = document.getElementById("member-skills");
+  var memberSkillsBtn = document.getElementById("member-skills-btn");
+  var memberSkillsClose = document.getElementById("member-skills-close");
+
+  memberSkillsBtn.onclick = function() {
+    memberSkills.style.display = "block";
+  }
+
+  memberSkillsClose.onclick = function() {
+    memberSkills.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == memberSkills) {
+      memberSkills.style.display = "none";
     }
-}  
+  }
+}
